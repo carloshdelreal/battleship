c28a678cf099fb7b9667f80145824cf7693c4fe9
trox115 <tofernandes67@gmail.com>
2019-12-05 23:32:05 +0000

    added a possible solution for the Board

--- src/factories/gameboard.js
+++ src/factories/gameboard.js
@@ -1,3 +1,34 @@
-const GameBoard = () => {};
+import Ship from './ship';
 
+const GameBoard = () => {
+  let board = [];
+  for (let i = 0; i < 10; i++) {
+    board.push(new Array(10).fill(0));
+  }
+  return board;
+};
+
+const randomShip = (ship) => {
+  const direction = ['v', 'h'];
+  const randomX = Math.floor(Math.random() * (10 - 0 + 1)) + 0;
+  const randomY = Math.floor(Math.random() * (10 - 0 + 1)) + 0;
+  const length = ship.length;
+  const randomdirection =
+    direction[Math.floor(Math.random() * (1 - 0 + 1)) + 0];
+  let endCoordinate = 0;
+  if (randomdirection == 'v') {
+    endCoordinate = randomY + length - 1;
+  } else {
+    endCoordinate = randomX + length - 1;
+  }
+
+  return { randomX, randomY, endCoordinate };
+};
+
+let board = new GameBoard();
+console.log(board);
+const ship = new Ship('cruiser');
+const random = randomShip(ship);
+board[random.randomX][random.randomY] = 1;
+console.log(board);
 export default GameBoard;
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,3 +1,32 @@
 import GameBoard from '../factories/gameboard';
+import expectExport from 'expect';
 
-test('Create the default GameBoard', () => {});
+test('Create the default GameBoard and check if it has 10 Rows', () => {
+  const b = new GameBoard();
+  expect(b.length).toEqual(10);
+});
+
+test('Create the default GameBoard and check if it has 10 columns', () => {
+  const b = new GameBoard();
+  for (let i = 0; i <= 9; i++) {
+    expect(b[i].length).toEqual(10);
+  }
+});
+
+test('Check if each board cell has been correctly initialized', () => {
+  const b = new GameBoard();
+  for (let i = 0; i <= 9; i++) {
+    for (let j = 0; j < 10; j++) {
+      expect(b[i][j]).toEqual(0);
+    }
+  }
+});
+
+test('Test if Board receives a new number in random direction', () => {
+  const b = new GameBoard();
+  for (let i = 0; i <= 9; i++) {
+    for (let j = 0; j < 10; j++) {
+      expect(b[i][j]).toEqual(0);
+    }
+  }
+});
