1f99f8888f1a01cc393a4523ad351003f0ef6ed6
Carlos Del Real <carloshdelreal@gmail.com>
2019-12-06 12:01:32 -0500

    create validation to the creation of the location of the ships

--- src/factories/gameboard.js
+++ src/factories/gameboard.js
@@ -9,12 +9,14 @@ const GameBoard = () => {
     patrolBoat: Ship('patrolBoat'),
   };
   const positions = {};
-  setPosition(
-    'battleship',
-    createShipCoords({ x: 0, y: 0 }, 'v', ship.battleship.length),
-  );
+
+  function createPositions() {}
+  setPosition('battleship', createShipCoords('battleship', 0, 0, 'v'));
+  setPosition('submarine', createShipCoords('submarine', 1, 0, 'h'));
+  setPosition('carrier'), createShipCoords('carrier', 0, 0, 'h');
 
   function setPosition(shipToSet, coords) {
+    if (coords == null) return null;
     for (let i = 0; coords.length > i; i += 1) {
       if (positions[coords[i].x] == null) {
         positions[coords[i].x] = {};
@@ -25,12 +27,14 @@ const GameBoard = () => {
     }
   }
 
-  function createShipCoords(startCoords, direction, length) {
-    const { x, y } = startCoords;
+  function createShipCoords(shipType, x, y, direction) {
+    const { length } = ship[shipType];
     let coords = [];
     if (direction == 'v') {
       for (let i = 0; length > i; i += 1) {
         if (x >= 10 || y + i >= 10) {
+          return null;
+        } else if (positions[x] != null && positions[x][y + i] != null) {
           return null;
         }
         coords.push({ x: x, y: y + i });
@@ -39,6 +43,8 @@ const GameBoard = () => {
     } else if (direction == 'h') {
       for (let i = 0; length > i; i += 1) {
         if (x + i >= 10 || y >= 10) {
+          return null;
+        } else if (positions[x] != null && positions[x + i][y] != null) {
           return null;
         }
         coords.push({ x: x + i, y: y });
@@ -50,21 +56,17 @@ const GameBoard = () => {
   return { positions, ship };
 };
 
-const randomShip = (ship) => {
+const randomShip = () => {
   const direction = ['v', 'h'];
   const randomX = Math.floor(Math.random() * (10 - 0 + 1)) + 0;
   const randomY = Math.floor(Math.random() * (10 - 0 + 1)) + 0;
-  const length = ship.length;
+
   const randomdirection =
     direction[Math.floor(Math.random() * (1 - 0 + 1)) + 0];
-  let endCoordinate = 0;
-  if (randomdirection == 'v') {
-    endCoordinate = randomY + length - 1;
-  } else {
-    endCoordinate = randomX + length - 1;
-  }
 
-  return { randomX, randomY, endCoordinate, randomdirection };
+  const directionStr = direction[randomdirection];
+
+  return { randomX, randomY, directionStr };
 };
 
 export default GameBoard;
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -14,6 +14,16 @@ test('set battleship position 0,0 vertical', () => {
       2: { battleship: 2 },
       3: { battleship: 3 },
     },
+
+    1: {
+      0: { submarine: 0 },
+    },
+    2: {
+      0: { submarine: 1 },
+    },
+    3: {
+      0: { submarine: 2 },
+    },
   };
   expect(gameBoard.positions).toEqual(expected);
 });
